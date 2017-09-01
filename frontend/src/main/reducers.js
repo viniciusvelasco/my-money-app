478219fc6c3589eb2b129de5946febe34965d8f2
Vinicius Alves Velasco <vinicius.velasco@outlook.com>
2017-08-31 23:43:08 -0400

    Impelementação do Redux no dashboard

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
+import DashboardReducer from '../dashboard/dashboardReducer'
+
 const rootReducer = combineReducers({
-  dashboard: () => ({sumary: {credit: 100, debt: 50}})
+  dashboard: DashboardReducer
 })
 
 export default rootReducer
