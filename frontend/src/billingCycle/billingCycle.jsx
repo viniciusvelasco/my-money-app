68cf17785908af26417e879a06578a4cc969d22b
Vinicius Alves Velasco <vinicius.velasco@outlook.com>
2017-09-01 21:32:01 -0400

    Implementação das tabs

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,19 +1,44 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
+import { selectTab } from '../common/tab/tabActions'
 
 class BillingCycle extends Component {
+  componentWillMount () {
+    this.props.selectTab('tabList')
+  }
   render () {
     return (
       <div>
         <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
         <Content>
-          Cliclos de Pagamentos
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label='Listar' icon='bars' target='tabList' />
+              <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+              <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+              <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+            </TabsHeader>
+            <TabsContent>
+              <TabContent id='tabList'><h1>Lista</h1></TabContent>
+              <TabContent id='tabCreate'><h1>Inserir</h1></TabContent>
+              <TabContent id='tabUpdate'><h1>Atualizar</h1></TabContent>
+              <TabContent id='tabDelete'><h1>Exluir</h1></TabContent>
+            </TabsContent>
+          </Tabs>
         </Content>
       </div>
     )
   }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycle)
