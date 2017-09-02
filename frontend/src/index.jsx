6cf5bac15a2cad3db8cdfd1ba3ae4ab055ef3643
Vinicius Alves Velasco <vinicius.velasco@outlook.com>
2017-09-02 13:50:49 -0400

    Impementação do redux-multi na billingCycleActions

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,14 +4,17 @@ import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import promisse from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import App from './main/app'
 import reducers from './main/reducers'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promisse)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi, thunk, promisse)(createStore)(reducers, devTools)
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
   , document.getElementById('app'))
+  
